50f8115cb46829025c92b30a29e6a7434d888978
Abby F <abby_faigao@yahoo.com>
2022-08-07 01:03:11 -0700

    Build todo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to [#1cb5e0]`,
+};
 
 function App() {
-  return <div></div>;
+  const [todos, setTodos] = useState(["Go to Work", "Go To Work"]);
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input type="text" placeholder="Add Todo" className={style.input} />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
