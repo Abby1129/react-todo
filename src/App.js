02919f49e285f54023fa407da5f21a3a53629f3b
Abby F <abby_faigao@yahoo.com>
2022-08-08 12:32:38 -0700

    Add partial css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Todo from "./Todo";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to [#1cb5e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
 };
 
 function App() {
